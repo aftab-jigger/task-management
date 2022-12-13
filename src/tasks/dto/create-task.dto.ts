ee819fc757c14f9b78aedb95ad87a57dd7d6b49c
Aftab Hussain <aftab@aftabs-mbp.home>
2022-12-13 20:43:31 +0500

    added Validation Pipe (IsNotEmpty)

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDTO {
+  @IsNotEmpty({ message: 'You must have to fill title' })
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
